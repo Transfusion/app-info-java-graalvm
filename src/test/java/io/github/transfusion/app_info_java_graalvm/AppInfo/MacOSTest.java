855b94e011e0a8da9a9b3ca208be4bb7a9156b94
Bryan Kok <bryan.wyern1@gmail.com>
2022-07-14 04:25:53 +0800

    Add in #macOS when has a signed app

--- src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/MacOSTest.java
+++ src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/MacOSTest.java
@@ -67,4 +67,54 @@ public class MacOSTest {
         subject.clear();
         ctx.close();
     }
+
+    @Test
+    void signedApp() {
+        Context ctx = createContext();
+        String resourceName = "apps/macos-signed.zip";
+        String absolutePath = getResourcesAbsolutePath(resourceName);
+
+        MacOS subject = MacOS.from(ctx, absolutePath);
+        Assertions.assertEquals(subject.os(), "macOS");
+        Assertions.assertTrue(subject.macos());
+        Assertions.assertFalse(subject.iphone());
+        Assertions.assertFalse(subject.ipad());
+        Assertions.assertFalse(subject.universal());
+
+        Assertions.assertEquals(subject.file(), absolutePath);
+        Assertions.assertEquals(subject.build_version(), "1");
+        Assertions.assertEquals(subject.release_version(), "1.0");
+        Assertions.assertEquals(subject.name(), "GuiApp");
+        Assertions.assertEquals(subject.bundle_name(), "GuiApp");
+        Assertions.assertNull(subject.display_name());
+        Assertions.assertEquals(subject.identifier(), "com.icyleaf.macos.GUIApp");
+        Assertions.assertEquals(subject.bundle_id(), "com.icyleaf.macos.GUIApp");
+        Assertions.assertEquals(subject.device_type(), "macOS");
+        Assertions.assertEquals(subject.min_os_version(), "11.3");
+        Assertions.assertEquals(subject.min_system_version(), "11.3");
+
+        Value hashSubscriptLambda = subject.getContext().eval("ruby", "-> recv, arg { recv[arg] }");
+        Value res = hashSubscriptLambda.execute(subject.info().getValue(), "CFBundleVersion");
+        Assertions.assertEquals(res.asString(), "1");
+
+        res = hashSubscriptLambda.execute(subject.info().getValue(), "CFBundleShortVersionString");
+        Assertions.assertEquals(res.asString(), "1.0");
+
+        Assertions.assertArrayEquals(subject.archs(), new String[]{"x86_64", "arm64"});
+        Assertions.assertEquals(subject.release_type(), "Release");
+
+        Assertions.assertFalse(subject.stored_question());
+
+        // it { expect(subject.info).to be_kind_of AppInfo::InfoPlist }
+
+        Assertions.assertTrue(subject.mobileprovision_question());
+        Assertions.assertEquals(subject.team_name(), "Samuel Sharps");
+        Assertions.assertEquals(subject.profile_name(), "Layouts");
+        Assertions.assertNotNull(subject.expired_date());
+        Assertions.assertNotNull(subject.distribution_name());
+        Assertions.assertNull(subject.icons_(true));
+
+        subject.clear();
+        ctx.close();
+    }
 }
