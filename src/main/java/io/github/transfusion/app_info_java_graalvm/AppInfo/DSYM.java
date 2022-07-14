7b385c1f1620b76e7aa7c1c89b6a1c0e01284fa8
Bryan Kok <bryan.wyern1@gmail.com>
2022-07-14 14:40:00 +0800

    Add dSYM #SingleMachO test

---
+++ src/main/java/io/github/transfusion/app_info_java_graalvm/AppInfo/DSYM.java
@@ -0,0 +1,75 @@
+package io.github.transfusion.app_info_java_graalvm.AppInfo;
+
+import io.github.transfusion.app_info_java_graalvm.AbstractPolyglotAdapter;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+public abstract class DSYM extends AbstractPolyglotAdapter {
+
+    public static DSYM from(Context polyglot, String path) {
+        polyglot.eval("ruby", "require 'app-info'");
+        return polyglot.eval("ruby", "AppInfo::DSYM.new('" + path + "')").as(DSYM.class);
+    }
+
+    public abstract String file_type();
+
+    public abstract String object();
+
+    /**
+     * @return MachO::MachOFile
+     */
+    public abstract Value macho_type();
+
+    public abstract DSYM.MachO[] machos();
+
+    public abstract String release_version();
+
+    public abstract String build_version();
+
+    public abstract String identifier();
+
+    public abstract String bundle_id();
+
+    /**
+     * @return a Hash; @info ||= CFPropertyList.native_types(CFPropertyList::List.new(file: info_path).value)
+     */
+    public abstract Value info();
+
+    public abstract String info_path();
+
+    public abstract String app_path();
+
+    public static abstract class MachO extends AbstractPolyglotAdapter {
+        public abstract String cpu_name();
+
+        public abstract String cpu_type();
+
+        public abstract String type();
+
+        public abstract String uuid();
+
+        public abstract String debug_id();
+
+        public abstract Long size();
+
+        public String humanSize() {
+            Context ctx = getContext();
+            Value callSize = ctx.eval("ruby", "-> recv, arg { recv.size(human_size: arg) }");
+            Value res = callSize.execute(getValue(), true);
+            return res.asString();
+        }
+
+        /**
+         * @return MachO::Headers::MachHeader64
+         */
+        public abstract Value header();
+    }
+
+    public abstract String contents();
+
+    public void clear() {
+        Context ctx = getContext();
+        Value v = ctx.eval("ruby", "-> recv { recv.clear! }");
+        v.execute(getValue());
+    }
+}
---
+++ src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/DSYMTest.java
@@ -0,0 +1,50 @@
+package io.github.transfusion.app_info_java_graalvm.AppInfo;
+
+import org.graalvm.polyglot.Context;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.github.transfusion.app_info_java_graalvm.AppInfo.Utilities.getResourcesAbsolutePath;
+
+public class DSYMTest {
+    private Context createContext() {
+        Context ctx = Context.newBuilder().allowAllAccess(true).build();
+        ctx.eval("ruby", "Encoding.default_external = 'ISO-8859-1'");
+        ctx.eval("ruby", "require 'app-info'");
+        return ctx;
+    }
+
+    @Test
+    void singleMachO() {
+        Context ctx = createContext();
+        String resourceName = "dsyms/single_ios.dSYM.zip";
+        String absolutePath = getResourcesAbsolutePath(resourceName);
+
+        DSYM subject = DSYM.from(ctx, absolutePath);
+
+        Assertions.assertEquals(subject.file_type(), "dSYM");
+        Assertions.assertEquals(subject.object(), "iOS");
+
+//        it { expect(subject.macho_type).to be_a ::MachO::MachOFile }
+        Assertions.assertFalse(subject.macho_type().isNull());
+        Assertions.assertEquals(subject.release_version(), "1.0");
+        Assertions.assertEquals(subject.build_version(), "1");
+
+        Assertions.assertEquals(subject.identifier(), "com.icyleaf.iOS");
+        Assertions.assertEquals(subject.bundle_id(), "com.icyleaf.iOS");
+
+        Assertions.assertEquals(subject.machos().length, 1);
+
+        Assertions.assertEquals(subject.machos()[0].type(), "dsym");
+        Assertions.assertEquals(subject.machos()[0].uuid(), "ea9bbf2d-bfdd-3ce0-85b2-1cbe7152fca5");
+        Assertions.assertEquals(subject.machos()[0].cpu_type(), "arm64");
+        Assertions.assertEquals(subject.machos()[0].cpu_name(), "arm64");
+        Assertions.assertEquals(subject.machos()[0].size(), 866911);
+        Assertions.assertEquals(subject.machos()[0].humanSize(), "846.59 KB");
+
+        // it { expect(subject.machos[0].to_h).to eq data }
+        // no need to test this since we can get the raw value with getValue() anyways
+        subject.clear();
+        ctx.close();
+    }
+}
