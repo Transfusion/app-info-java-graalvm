a725246d884eb03ed8849bf049c41aa167b29607
Bryan Kok <bryan.wyern1@gmail.com>
2022-07-14 18:04:45 +0800

    Add dSYM #MultiMachO test

--- src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/DSYMTest.java
+++ src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/DSYMTest.java
@@ -1,5 +1,6 @@
 package io.github.transfusion.app_info_java_graalvm.AppInfo;
 
+import io.github.transfusion.app_info_java_graalvm.MachO.FatFile;
 import io.github.transfusion.app_info_java_graalvm.MachO.MachOFile;
 import org.graalvm.polyglot.Context;
 import org.junit.jupiter.api.Assertions;
@@ -52,6 +53,46 @@ public class DSYMTest {
 
     @Test
     void multiMachO() {
+        Context ctx = createContext();
+        String resourceName = "dsyms/multi_ios.dSYM.zip";
+        String absolutePath = getResourcesAbsolutePath(resourceName);
+
+        DSYM subject = DSYM.from(ctx, absolutePath);
+
+        Assertions.assertEquals(subject.file_type(), "dSYM");
+        Assertions.assertEquals(subject.object(), "iOS");
+
+        Assertions.assertFalse(subject.macho_type_().getValue().isNull());
+//        it { expect(subject.macho_type).to be_a ::MachO::MachOFile }
+        Assertions.assertTrue(subject.macho_type_() instanceof FatFile);
+        Assertions.assertEquals(subject.release_version(), "1.0");
+        Assertions.assertEquals(subject.build_version(), "2");
+
+        Assertions.assertEquals(subject.identifier(), "com.icyleaf.iOS");
+        Assertions.assertEquals(subject.bundle_id(), "com.icyleaf.iOS");
+
+        Assertions.assertEquals(subject.machos().length, 2);
+
+        Assertions.assertEquals(subject.machos()[0].type(), "dsym");
+        Assertions.assertEquals(subject.machos()[0].uuid(), "26dfc15d-bdce-351f-b5de-6ee9f5dd6d85");
+        Assertions.assertEquals(subject.machos()[0].cpu_type(), "arm");
+        Assertions.assertEquals(subject.machos()[0].cpu_name(), "armv7");
+        Assertions.assertEquals(subject.machos()[0].size(), 866526);
+        Assertions.assertEquals(subject.machos()[0].humanSize(), "846.22 KB");
+
+        // it { expect(subject.machos[0].to_h).to eq data[0] }
+
+        Assertions.assertEquals(subject.machos()[1].type(), "dsym");
+        Assertions.assertEquals(subject.machos()[1].uuid(), "17f58291-dd25-3fc8-9417-ccbe8163d33e");
+        Assertions.assertEquals(subject.machos()[1].cpu_type(), "arm64");
+        Assertions.assertEquals(subject.machos()[1].cpu_name(), "arm64");
+        Assertions.assertEquals(subject.machos()[1].size(), 866911);
+        Assertions.assertEquals(subject.machos()[1].humanSize(), "846.59 KB");
+
+        // it { expect(subject.machos[1].to_h).to eq data[1] }
+
+        subject.clear();
+        ctx.close();
 
     }
 }
