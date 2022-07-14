1d5564deb29e0c90cdb56d55628e5600fa5a7018
Bryan Kok <bryan.wyern1@gmail.com>
2022-07-14 13:05:50 +0800

    Add MobileProvision macOS AppStore test case

--- src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/MobileProvisionTest.java
+++ src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/MobileProvisionTest.java
@@ -124,8 +124,6 @@ public class MobileProvisionTest {
         Assertions.assertNotNull(subject.team_identifier());
         Assertions.assertNotNull(subject.team_name());
         Assertions.assertNotNull(subject.profile_name());
-        System.out.println(subject.created_date());
-        System.out.println(subject.expired_date());
         Assertions.assertTrue(subject.created_date().isEqual(ZonedDateTime.parse("2020-07-21T16:45:25+08:00")));
         Assertions.assertTrue(subject.expired_date().isEqual(ZonedDateTime.parse("2020-10-24T16:40:28+08:00")));
 
@@ -177,12 +175,55 @@ public class MobileProvisionTest {
 
         // additional testing for MobileProvision::DeveloperCertificate
         Assertions.assertEquals(subject.developer_certs()[0].name(), "Apple Development: Shen Wang (A3Z3NZQ8V3)");
-        System.out.println(subject.developer_certs()[0].created_date());
-        System.out.println(subject.developer_certs()[0].expired_date());
         Assertions.assertTrue(subject.developer_certs()[0].created_date().isEqual(ZonedDateTime.parse("2021-05-28T13:33:21Z")));
         Assertions.assertTrue(subject.developer_certs()[0].expired_date().isEqual(ZonedDateTime.parse("2020-05-28T13:33:21Z")));
 
         Assertions.assertArrayEquals(subject.enabled_capabilities(), new String[]{"System Extension", "Maps", "Network Extensions", "Apple Pay", "Associated Domains", "Personal VPN", "AutoFill Credential Provider", "ClassKit", "Low Latency HLS", "iCloud", "MDM Managed Associated Domains", "Sign In with Apple", "Custom Network Protocol"});
+
+        ctx.close();
+    }
+
+    @Test
+    void macOSAppStore() {
+        Context ctx = createContext();
+        String resourceName = "mobileprovisions/macos_appstore.provisionprofile";
+        String absolutePath = getResourcesAbsolutePath(resourceName);
+
+        MobileProvision subject = MobileProvision.from(ctx, absolutePath);
+        Assertions.assertNull(subject.devices());
+
+        Assertions.assertEquals(subject.platform(), "macos");
+        Assertions.assertArrayEquals(subject.platforms(), new String[]{"macos"});
+
+        Assertions.assertNotNull(subject.name());
+        Assertions.assertNotNull(subject.app_name());
+
+        Assertions.assertEquals(subject.type(), "appstore");
+        Assertions.assertFalse(subject.development_question());
+        Assertions.assertFalse(subject.adhoc_question());
+        Assertions.assertTrue(subject.appstore_question());
+        Assertions.assertFalse(subject.inhouse_question());
+        Assertions.assertFalse(subject.enterprise_question());
+        Assertions.assertNotNull(subject.team_identifier());
+        Assertions.assertNotNull(subject.team_name());
+
+        Assertions.assertNotNull(subject.profile_name());
+        Assertions.assertTrue(subject.created_date().isEqual(ZonedDateTime.parse("2020-07-21T16:46:19+08:00")));
+        Assertions.assertTrue(subject.expired_date().isEqual(ZonedDateTime.parse("2020-10-24T16:40:28+08:00")));
+
+        Value hashSubscriptLambda = subject.getContext().eval("ruby", "-> recv, arg { recv[arg] }");
+        Value res = hashSubscriptLambda.execute(subject.entitlements(), "com.apple.developer.maps");
+        Assertions.assertTrue(res.asBoolean());
+
+        Assertions.assertEquals(1, subject.developer_certs().length);
+
+        // additional testing for MobileProvision::DeveloperCertificate
+        Assertions.assertEquals(subject.developer_certs()[0].name(), "Apple Distribution: Niceliving (Beijing) Technology Co., Ltd. (WKR87TTKML)");
+
+        Assertions.assertTrue(subject.developer_certs()[0].created_date().isEqual(ZonedDateTime.parse("2020-10-24T08:40:28Z")));
+        Assertions.assertTrue(subject.developer_certs()[0].expired_date().isEqual(ZonedDateTime.parse("2019-10-25T08:40:28Z")));
+
+        Assertions.assertArrayEquals(subject.enabled_capabilities(), new String[]{"In-App Purchase", "GameKit", "System Extension", "Maps", "Network Extensions", "Apple Pay", "Associated Domains", "Personal VPN", "AutoFill Credential Provider", "ClassKit", "Low Latency HLS", "iCloud", "MDM Managed Associated Domains", "Sign In with Apple", "Custom Network Protocol"});
 
         ctx.close();
     }
