5b734401069632d6637de49c3ae59af8e8e278c3
Bryan Kok <bryan.wyern1@gmail.com>
2022-07-14 12:48:08 +0800

    Add MobileProvision iOS AppStore test case

--- src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/MobileProvisionTest.java
+++ src/test/java/io/github/transfusion/app_info_java_graalvm/AppInfo/MobileProvisionTest.java
@@ -96,33 +96,47 @@ public class MobileProvisionTest {
         Assertions.assertEquals(subject.developer_certs()[0].name(), "Apple Distribution: Niceliving (Beijing) Technology Co., Ltd. (WKR87TTKML)");
         Assertions.assertTrue(subject.developer_certs()[0].created_date().isEqual(ZonedDateTime.parse("2020-10-24T08:40:28Z")));
         Assertions.assertTrue(subject.developer_certs()[0].expired_date().isEqual(ZonedDateTime.parse("2019-10-25T08:40:28Z")));
-        Assertions.assertArrayEquals(subject.enabled_capabilities(), new String[]{"In-App Purchase",
-                "GameKit",
-                "Access WiFi Information",
-                "App Groups",
-                "Apple Pay",
-                "Associated Domains",
-                "AutoFill Credential Provider",
-                "ClassKit",
-                "Data Protection",
-                "HealthKit",
-                "HomeKit",
-                "Hotspot",
-                "iCloud",
-                "Inter-App Audio",
-                "Multipath",
-                "Network Extensions",
-                "NFC Tag Reading",
-                "Push Notifications",
-                "SiriKit",
-                "Personal VPN",
-                "Wireless Accessory Configuration",
-                "Wallet",
-                "Low Latency HLS",
-                "App Attest",
-                "Extended Virtual Address Space",
-                "Fonts",
-                "MDM Managed Associated Domains",
-                "Sign In with Apple"});
+        Assertions.assertArrayEquals(subject.enabled_capabilities(), new String[]{"In-App Purchase", "GameKit", "Access WiFi Information", "App Groups", "Apple Pay", "Associated Domains", "AutoFill Credential Provider", "ClassKit", "Data Protection", "HealthKit", "HomeKit", "Hotspot", "iCloud", "Inter-App Audio", "Multipath", "Network Extensions", "NFC Tag Reading", "Push Notifications", "SiriKit", "Personal VPN", "Wireless Accessory Configuration", "Wallet", "Low Latency HLS", "App Attest", "Extended Virtual Address Space", "Fonts", "MDM Managed Associated Domains", "Sign In with Apple"});
+    }
+
+    @Test
+    void iOSAppStore() {
+        Context ctx = createContext();
+        String resourceName = "mobileprovisions/ios_appstore.mobileprovision";
+        String absolutePath = getResourcesAbsolutePath(resourceName);
+
+        MobileProvision subject = MobileProvision.from(ctx, absolutePath);
+
+        Assertions.assertNull(subject.devices());
+        Assertions.assertEquals(subject.platform(), "ios");
+        Assertions.assertArrayEquals(subject.platforms(), new String[]{"ios"});
+
+        Assertions.assertNotNull(subject.name());
+        Assertions.assertNotNull(subject.app_name());
+
+        Assertions.assertEquals(subject.type(), "appstore");
+
+        Assertions.assertFalse(subject.development_question());
+        Assertions.assertFalse(subject.adhoc_question());
+        Assertions.assertTrue(subject.appstore_question());
+        Assertions.assertFalse(subject.inhouse_question());
+        Assertions.assertFalse(subject.enterprise_question());
+        Assertions.assertNotNull(subject.team_identifier());
+        Assertions.assertNotNull(subject.team_name());
+        Assertions.assertNotNull(subject.profile_name());
+        System.out.println(subject.created_date());
+        System.out.println(subject.expired_date());
+        Assertions.assertTrue(subject.created_date().isEqual(ZonedDateTime.parse("2020-07-21T16:45:25+08:00")));
+        Assertions.assertTrue(subject.expired_date().isEqual(ZonedDateTime.parse("2020-10-24T16:40:28+08:00")));
+
+
+        Assertions.assertEquals(1, subject.developer_certs().length);
+
+        // additional testing for MobileProvision::DeveloperCertificate
+        Assertions.assertEquals(subject.developer_certs()[0].name(), "Apple Distribution: Niceliving (Beijing) Technology Co., Ltd. (WKR87TTKML)");
+
+        Assertions.assertTrue(subject.developer_certs()[0].created_date().isEqual(ZonedDateTime.parse("2020-10-24T08:40:28Z")));
+        Assertions.assertTrue(subject.developer_certs()[0].expired_date().isEqual(ZonedDateTime.parse("2019-10-25T08:40:28Z")));
+        Assertions.assertArrayEquals(subject.enabled_capabilities(), new String[]{"In-App Purchase", "GameKit", "Access WiFi Information", "App Groups", "Apple Pay", "Associated Domains", "AutoFill Credential Provider", "ClassKit", "Data Protection", "HealthKit", "HomeKit", "Hotspot", "iCloud", "Inter-App Audio", "Multipath", "Network Extensions", "NFC Tag Reading", "Push Notifications", "SiriKit", "Personal VPN", "Wireless Accessory Configuration", "Wallet", "Low Latency HLS", "App Attest", "Extended Virtual Address Space", "Fonts", "MDM Managed Associated Domains", "Sign In with Apple"});
     }
 }
